7152668374526088f50508067e5a263a43a56309
Christopher Torgalson <manager@bedlamhotel.com>
2015-05-31 13:54:04 +0200

    Updates install script to latest

--- installsite.sh
+++ installsite.sh
@@ -1,5 +1,4 @@
 SITENAME="d7"
-REPOSITORY="https://github.com/ctorgalson/osd7"
+REPOSITORY="https://github.com/ctorgalson/openshift-d7"
 
-rhc app create $SITENAME php-5.3 mysql-5.1 cron --from-code=$REPOSITORY -e ./.openshift/environment_variables --no-git
-
+rhc app create $SITENAME php-5.5 mysql-5.1 cron --from-code=$REPOSITORY -e ./.openshift/environment_variables --no-git
